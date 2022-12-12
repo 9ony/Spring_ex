950c48ea0c892b3200541825102e1776dd34602d
9ony <darsta7@gmail.com>
2022-12-13 00:29:24 +0900

    페이징 블록 코드 리팩토링 및 검색기능 추가 jstl로 작성한 로직을 pagevo에 작성 View에는 최대한 로직관련된걸 줄이기위해 검색기능은 글제목 , 작성자 , 글내용으로 구분

--- SpingMVC02/src/main/java/com/board/model/PagingVO.java
+++ SpingMVC02/src/main/java/com/board/model/PagingVO.java
@@ -1,5 +1,10 @@
 package com.board.model;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import javax.servlet.http.HttpSession;
+
 import lombok.Data;
 
 /*페이징 처리 및 검색기능 모듈화*/
@@ -7,7 +12,7 @@ import lombok.Data;
 public class PagingVO {
 	//페이징 처리 관련 프로퍼티
 	private int cpage;
-	private int pageSize;
+	private int pageSize=5;
 	private int totalCount;
 	private int pageCount;
 	
@@ -25,7 +30,10 @@ public class PagingVO {
 	private String findKeyword; //검색 키워드
 	
 	//페이징 처리 초기설정 메서드
-	public void init() {
+	public void init(HttpSession ses) {
+		if(ses!=null) {
+			ses.setAttribute("pageSize", pageSize);
+		}
 		pageCount=(totalCount-1)/pageSize+1;
 		if(cpage<1) {
 			cpage=1;
@@ -60,6 +68,62 @@ public class PagingVO {
 
 		
 	}
-	
+	public String getPageNavi(String myctx,String loc,String userAgent) {
+		//myctx = 컨텍스트명
+		//loc   = board/list
+		//userAgent = 클라이언트 브라우저 파악
+		//검색관련--------------
+		if(findType==null) {//검색어가 넘어오지 않을 경우
+			findType="";
+			findKeyword="";			
+		}else {
+			//브라우저 IE일경우 검색어 한글 처리하기
+			if(userAgent.indexOf("MSIE")>-1||userAgent.indexOf("Trident")>-1) {
+				try {
+				findKeyword=URLEncoder.encode(findKeyword,"UTF-8");
+				}catch(UnsupportedEncodingException e) {
+					System.out.println(e);
+				}
+			}
+			
+		}
+		/////////////////////////////
+		String link=myctx+"/"+loc; // "/multiweb/board/list"
+		String qStr="?pageSize="+pageSize+"&findType="+findType+"&findKeyword="+findKeyword;
+		link+=qStr;
+		/////////////////////////////
+		String str="";
+		StringBuilder buf = new StringBuilder();
+		buf.append("<ul class='pagination justify-content-center'>");
+		if(prevBlock>0) {
+			buf.append("<li class='page-item'>")// "/multiweb/board/list?cpage=5"
+				.append("<a class='page-link' href='"+link+"&cpage="+prevBlock+"'>")
+				.append("Prev")
+				.append("</a>")
+				.append("</li>");
+		}
+		for(int i=prevBlock+1; i<=nextBlock-1 && i<=pageCount ;i++) {
+			String css=(i==cpage)? "active":"";
+			
+			buf.append("<li class='page-item "+css+"'>");
+			buf.append("<a class='page-link' href='"+link+"&cpage="+i+"'>");
+			buf.append(i);
+			buf.append("</a>");
+			buf.append("</li>");
+		}
+		
+		if(nextBlock<=pageCount) {
+			buf.append("<li class='page-item'>")// "/multiweb/board/list?cpage=5"
+				.append("<a class='page-link' href='"+link+"&cpage="+nextBlock+"'>")
+				.append("Next")
+				.append("</a>")
+				.append("</li>");
+		}
+		
+		buf.append("</ul>");
+		str=buf.toString();
+		//System.out.println(str);
+		return str;
+	}
 	
 }
